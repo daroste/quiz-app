4fd17057207a2ad93dc6449d0fd1733a6f91badd
Daniel-Ronald Steinbrueck <daniel.steinbrueck@me.com>
2022-09-02 09:14:42 +0200

    changed css style

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const name = "Daniel";
-console.log(name);
+console.clear();
+let carrots = 2;
+const kidhelpedsdoingthedishes = false;
+let extraCarots = false ? 2 : -1;
+console.log("extraCarrots = " + extraCarots);
+carrots = carrots + extraCarots;
+console.log("carrots = +carrots);
