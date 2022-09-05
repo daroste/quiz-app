f8d4cbfba6c6d8f5ad4eac560eff9b33045363e7
Daniel-Ronald Steinbrueck <daniel.steinbrueck@me.com>
2022-09-05 09:03:09 +0200

    work on css

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-console.clear();
-let carrots = 2;
-const kidhelpedsdoingthedishes = false;
-let extraCarots = false ? 2 : -1;
-console.log("extraCarrots = " + extraCarots);
-carrots = carrots + extraCarots;
-console.log("carrots = +carrots);
+console.log(Guten Morgen);
