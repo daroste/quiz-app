337db3cbbe00fa3e4d5e2a56d125d21a575a0d72
Daniel-Ronald Steinbrueck <daniel.steinbrueck@me.com>
2022-09-08 17:02:39 +0200

    add new site create_card

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-function hello() {
-  alert(Hello);
+function myFunction() {
+  var x = document.getElementById("myanswer");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
+function myFunction2() {
+  var x = document.getElementById("myanswer2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("Test");
+});
